c5a336a1e9e49d834aeb08ea4f7d3f918b6313d8
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-07-27 15:12:59 +0300

    Expand EmployeeDAO interface with new methods

--- src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeDAO.java
@@ -6,5 +6,8 @@ import java.util.List;
 
 public interface EmployeeDAO {
     List<Employee> findAll();
+    Employee findById(int id);
+    Employee save(Employee employee);
+    void deleteById(int id);
 
 }
