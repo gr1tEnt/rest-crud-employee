c905c894d650a47df48e95baa4646da5b6a4f085
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-07-27 13:12:04 +0300

    Create findAll method in EmployeeDAOJpaImpl

--- src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeDAO.java
@@ -1,5 +1,10 @@
 package com.gr1tEnt.cruddemo.dao;
 
+import com.gr1tEnt.cruddemo.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeDAO {
+    List<Employee> findAll();
 
 }
--- src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -1,8 +1,12 @@
 package com.gr1tEnt.cruddemo.dao;
 
+import com.gr1tEnt.cruddemo.entity.Employee;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class EmployeeDAOJpaImpl implements EmployeeDAO {
@@ -12,5 +16,11 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
     @Autowired
     public EmployeeDAOJpaImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("FROM Employee", Employee.class);
+        return query.getResultList();
     }
 }
