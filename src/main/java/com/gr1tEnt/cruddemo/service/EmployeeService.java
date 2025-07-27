f6b935ef6ff04e2fca91ba7965c601d605799940
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-07-27 14:57:31 +0300

    Create EmployeeService interface

---
+++ src/main/java/com/gr1tEnt/cruddemo/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.gr1tEnt.cruddemo.service;
+
+import com.gr1tEnt.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+}
