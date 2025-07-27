bfd72b2bbc120e524355667389d6c88cece0427b
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-07-27 16:01:26 +0300

    Add method getEmployee method in EmployeeRestController

--- src/main/java/com/gr1tEnt/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/gr1tEnt/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.gr1tEnt.cruddemo.rest;
 import com.gr1tEnt.cruddemo.entity.Employee;
 import com.gr1tEnt.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,15 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employee;
     }
 }
