8228712bb98d12436ce5fdba4b9bafecbdf447f6
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-07-27 12:28:33 +0300

    Initial commit

---
+++ src/test/java/com/gr1tEnt/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr1tEnt.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
