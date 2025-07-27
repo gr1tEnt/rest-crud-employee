649818ec48bf2a8641c5fcb8e88fe22d50b9c748
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-07-27 15:14:25 +0300

    Add save method to EmployeeDAOJpaImpl class

--- src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -28,4 +28,10 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
     public Employee findById(int id) {
         return entityManager.find(Employee.class, id);
     }
+
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
+
 }
