21b1df896b367dc2b953865cf4cc6bfcde93e8c9
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-08-03 03:08:52 +0300

    Change basic paths for entity during using data-rest

--- src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/gr1tEnt/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.gr1tEnt.cruddemo.dao;
 
 import com.gr1tEnt.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
