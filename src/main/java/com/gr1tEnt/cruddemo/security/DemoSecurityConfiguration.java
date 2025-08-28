70751b764ee420a9acc46832c998f3fd4feaa1f2
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-08-28 20:03:56 +0300

    JDBC Authentication - Plain Text

--- src/main/java/com/gr1tEnt/cruddemo/security/DemoSecurityConfiguration.java
+++ src/main/java/com/gr1tEnt/cruddemo/security/DemoSecurityConfiguration.java
@@ -5,35 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("John")
-                .password("{noop}pass123")
-                .roles("EMPLOYEE")
-                .build();
+    public UserDetailsManager detailsManager(DataSource dataSource) {
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}pass123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails alex = User.builder()
-                .username("alex")
-                .password("{noop}pass123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, alex);
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -57,3 +41,27 @@ public class DemoSecurityConfiguration {
         return http.build();
     }
 }
+
+/*
+@Bean
+public InMemoryUserDetailsManager userDetailsManager() {
+    UserDetails john = User.builder()
+            .username("John")
+            .password("{noop}pass123")
+            .roles("EMPLOYEE")
+            .build();
+
+    UserDetails mary = User.builder()
+            .username("mary")
+            .password("{noop}pass123")
+            .roles("EMPLOYEE", "MANAGER")
+            .build();
+
+    UserDetails alex = User.builder()
+            .username("alex")
+            .password("{noop}pass123")
+            .roles("EMPLOYEE", "MANAGER", "ADMIN")
+            .build();
+
+    return new InMemoryUserDetailsManager(john, mary, alex);
+}*/
